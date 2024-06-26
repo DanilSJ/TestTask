3645022e8e280ebc1215662ff17dbec3317fff57
Danil_Chagarnoy <danilchagarnoy@mail.ru>
2024-06-26 21:46:30 +0300

    script

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+from PIL import Image
+
+def collect_images_from_folders(folder_list):
+    images = []
+    for folder in folder_list:
+        for root, dirs, files in os.walk(folder):
+            for file in files:
+                if file.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
+                    file_path = os.path.join(root, file)
+                    img = Image.open(file_path)
+                    images.append(img)
+    return images
+
+def save_images_as_tiff(images, output_file):
+    if not images:
+        print("Нет изображений")
+        return
+
+    images[0].save(
+        output_file,
+        save_all=True,
+        append_images=images[1:],
+        compression="tiff_deflate"
+    )
+    print(f"Сохраненно {len(images)} изображений в {output_file}")
+
+def main(folder_list, output_file):
+    images = collect_images_from_folders(folder_list)
+    save_images_as_tiff(images, output_file)
+
+if __name__ == "__main__":
+    folders = ['1369_12_Наклейки 3-D_3']
+    output_file = "Result.tif"
+    main(folders, output_file)
